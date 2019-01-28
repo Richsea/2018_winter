3477706138285412a2c116f51dd6d9c4145d4557
Richsea <201402377@cs-cnu.org>
2019-01-29 03:54:45 +0900

    파일 입력 완료
    파일을 읽어와서 int형 자료로 변환하는 과정까지 완료.
    
    ※ Insertion Sort를 진행할 때 읽어온 배열을 사용하도록 수정필요

--- Winter_2018_Insertion/Winter_2018_Insertion/FileIO.c
+++ Winter_2018_Insertion/Winter_2018_Insertion/FileIO.c
@@ -5,7 +5,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-int readFile() {
+int* readFile() {
 	fp = fopen("data02.txt", "r");
 
 	char* buffer;
@@ -24,13 +24,45 @@ int readFile() {
 	fseek(fp, 0, SEEK_SET);		// 파일 포인터를 파일의 처음으로 이동시킴
 	count = fread(buffer, size, 1, fp);	// 파일 크기만큼 값을 읽음
 
-	return 0;	
+	int i = 0;
+	int j = 0;
+	int loc = 0;
+	
+	int* data = malloc(size + 1);
+	memset(data, NULL, size + 1);
+	printf("%d\n", data);
+	char s1[10];
+	memset(s1, NULL, 10);
+	while (i < size)
+	{
+		
+		if (buffer[i] != ',')
+		{
+			s1[j] = buffer[i];
+			i++;
+			j++;
+			printf("%s", s1);
+		}
+		else
+		{
+			int temp = atoi(s1);
+			data[loc] = temp;
+			j = 0;
+			loc++;
+			i++;
+			memset(s1, NULL, 10);
+		}
+	}
+	data[loc] = atoi(s1);
+	fclose(fp);
+
+	return data;	
 }
 
 //fprintf와 fwrite 모두 사용할 때 fclose를 해줘야 변경사항이 적용된다.
 int writeFile(int* outputData[]) {
 	fp = fopen("data02_Insertion.txt", "wt");
-	
+
 	char answer[16];
 	int size = 0;
 	int temp = 0;
@@ -42,25 +74,23 @@ int writeFile(int* outputData[]) {
 
 	char* data = (char*)malloc(sizeof(char)*size);
 	memset(data, NULL, sizeof(char)*size);
+	temp = 0;
 
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < size-1; i++)
 	{
 		char s1[10];
-		printf("%d\n", outputData[i]);
 		sprintf(s1, "%d", outputData[i]);
-		printf("%s\n",s1);
 		strcat(s1,",");
 		strcat(data,s1);
 	}
 
-
-	printf("%s", data);
-
-	fprintf(fp,data);
-
-
-	//fprintf(fp, data);
-	//fwrite(*data, strlen(data), 1, fp);
+	char s1[10];
+	sprintf(s1, "%d", outputData[size-1]);
+	strcat(data, s1);
+	
+	//printf("%s", data);
+	fprintf(fp, data);
+	
 	fclose(fp);
 
 	return 0;
@@ -111,8 +141,4 @@ void append(char *dst, char c) {
 	while (*p != '\0')	p++;
 	*p = c;
 	*(p + 1) = '\0';
-}
-
-void addArray(int* arr) {
-	
 }
--- Winter_2018_Insertion/Winter_2018_Insertion/FileIO.h
+++ Winter_2018_Insertion/Winter_2018_Insertion/FileIO.h
@@ -4,10 +4,9 @@
 
 FILE *fp;
 
-int readFile(void);
+int* readFile(void);
 
 int writeFile(int* outputData[]);
 char* saveChar(int* outputData);
-void addArray(int* arr);
 
 void append(char* dst, char c);
